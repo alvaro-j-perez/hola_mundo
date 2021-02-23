3f40b7157ad51d1a0b5023cdb1fc9c3d2a2a552a
alvaro-j-perez <aperezg55@miumg.edu.gt>
2021-02-22 22:02:03 -0600

    mi primer repositorio del tercer semestre

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!! ");
+            Console.WriteLine("soy crack :)");
+        }
+    }
+}
